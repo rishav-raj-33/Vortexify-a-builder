922efbffeeb68b3bbe967a8d49d689eedc41a144
Rishav Raj <rishav3330@gmail.com>
2025-04-12 11:56:56 +0530

    brain-module setup done

---
+++ Brain Module/Vortexify/src/main/java/com/vortexify/brain/VortexifyApplication.java
@@ -0,0 +1,13 @@
+package com.vortexify.brain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VortexifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VortexifyApplication.class, args);
+	}
+
+}
