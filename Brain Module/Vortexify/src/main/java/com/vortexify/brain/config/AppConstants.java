8eebd9d0f13cca5ece3617bcffcb901c04ec1f25
Rishav Raj <rishav3330@gmail.com>
2025-04-21 21:34:47 +0530

    constants added

--- Brain Module/Vortexify/src/main/java/com/vortexify/brain/config/AppConstants.java
+++ Brain Module/Vortexify/src/main/java/com/vortexify/brain/config/AppConstants.java
@@ -9,6 +9,19 @@ public class AppConstants {
 	    SUCCESS,
 	    FAILED
 	};
+	
+	public static final String HOSTNAME="192.168.18.128";
+	public static final String USER_NAME="dev";
+	public static final String PASSWORD="root";
+	public static final String CLONE_DIR="D:\\Learning space\\Projects\\Vortexify-Parent\\Branch-Brain Module\\Heart Module\\python scripts\\Resource\\Clone\\";
+	public static final String TAR_DIR="D:\\Learning space\\Projects\\Vortexify-Parent\\Branch-Brain Module\\Heart Module\\python scripts\\Resource\\Docker-Images\\";
+	public static final String SCRIPT_DIR="D:\\Learning space\\Projects\\Vortexify-Parent\\Branch-Brain Module\\Heart Module\\python scripts\\";
+	public static final String CLONE_SCRIPT="clone.py";
+	public static final String BUILD_DOCKER_SCRIPT="buildDocker.py";
+	public static final String DEPLOY_SCRIPT="deloy.py";
+	public static final String STOP_SCRIPT="stopContainer.py";
+	public static final String REMOVE_DEPLOY_SCRIPT="removeDeploy.py";
+	
 
 	
 	
