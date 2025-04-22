c9c86b4008348d51324c073a70ba2694b6906359
Rishav Raj <rishav3330@gmail.com>
2025-04-23 00:16:28 +0530

    controllers code added

--- Brain Module/Vortexify/src/main/java/com/vortexify/brain/controllers/DeploymentInfoController.java
+++ Brain Module/Vortexify/src/main/java/com/vortexify/brain/controllers/DeploymentInfoController.java
@@ -1,16 +1,20 @@
 package com.vortexify.brain.controllers;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vortexify.brain.exception.DeploymentFailedException;
 import com.vortexify.brain.payloads.DeploymentSuccessResponse;
+import com.vortexify.brain.service.DeployService;
 import com.vortexify.brain.service.EntityService;
 
 @RestController
@@ -19,6 +23,9 @@ public class DeploymentInfoController {
  
 	@Autowired
 	private EntityService service;
+	
+	@Autowired
+	private DeployService deployServive;
 	
 	@GetMapping("/{userId")
 	public ResponseEntity<List<DeploymentSuccessResponse>> getAll(@PathVariable Long userId){
@@ -29,6 +36,12 @@ public class DeploymentInfoController {
 	@GetMapping("/{liveLink}")
 	public ResponseEntity<DeploymentSuccessResponse> getDeployment(@PathVariable String liveLink){
 		return new ResponseEntity<>(service.getDeploymentInfo(liveLink),HttpStatus.ACCEPTED);
+	}
+	
+	
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<Boolean> removeUser(@PathVariable Long userId) throws DeploymentFailedException, IOException, InterruptedException{
+		return new ResponseEntity<>(deployServive.removeUser(userId),HttpStatus.ACCEPTED);
 	}
 	
 	
--- Brain Module/Vortexify/src/main/java/com/vortexify/brain/controllers/TriggerController.java
+++ Brain Module/Vortexify/src/main/java/com/vortexify/brain/controllers/TriggerController.java
@@ -1,12 +1,47 @@
 package com.vortexify.brain.controllers;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.vortexify.brain.exception.DeploymentFailedException;
+import com.vortexify.brain.payloads.DeploymentSuccessResponse;
+import com.vortexify.brain.payloads.Request;
+import com.vortexify.brain.service.DeployService;
 
 @RestController
 @RequestMapping("/api/service")
 public class TriggerController {
+	
+	@Autowired
+	private DeployService service;
 
+	
+	@PostMapping("/deploy")
+	public ResponseEntity<DeploymentSuccessResponse> deploy(@RequestBody Request request) throws DeploymentFailedException, IOException, InterruptedException{
+		return new ResponseEntity<>(service.deploy(request),HttpStatus.CREATED);	
+	}
+	
+	
+	@PostMapping("/stop")
+	public ResponseEntity<DeploymentSuccessResponse> stop(@RequestBody Request request) throws DeploymentFailedException, IOException, InterruptedException{
+		return new ResponseEntity<>(service.stop(request),HttpStatus.CREATED);
+	}
+	
+	
+	@PostMapping("/remove")
+	public ResponseEntity<Boolean> remove(@RequestBody Request request) throws DeploymentFailedException, IOException, InterruptedException{
+		service.remove(request);
+		return new ResponseEntity<>(true,HttpStatus.ACCEPTED);
+	}
+	
+	
 	
 	
 	
